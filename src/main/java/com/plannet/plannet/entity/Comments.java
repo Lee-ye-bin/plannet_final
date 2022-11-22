418ebd6c82fcad76c46b2851a8e86d9c6a723ce2
lyb0623 <nnoonxnoonn@gmail.com>
2022-11-22 12:24:38 +0900

    .

--- src/main/java/com/plannet/plannet/entity/Comments.java
+++ src/main/java/com/plannet/plannet/entity/Comments.java
@@ -1,30 +1,30 @@
-package com.plannet.plannet.entity;
-
-import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-
-import javax.persistence.*;
-import java.sql.Date;
-import java.time.LocalDateTime;
-
-@Data
-@Entity
-public class Comments {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long commentNo;
-    @ManyToOne
-    @JoinColumn(nullable = false, name = "board_no")
-    private int boardNo;
-    @ManyToOne
-    @JoinColumn(name = "id")
-    private Member id;
-    @CreatedDate
-    private LocalDateTime writeDate;
-    @Column(nullable = false, length = 500)
-    private String detail;
-}
-
-
-
+//package com.plannet.plannet.entity;
+//
+//import lombok.Data;
+//import org.hibernate.annotations.CreationTimestamp;
+//import org.springframework.data.annotation.CreatedDate;
+//
+//import javax.persistence.*;
+//import java.sql.Date;
+//import java.time.LocalDateTime;
+//
+//@Data
+//@Entity
+//public class Comments {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long commentNo;
+//    @ManyToOne
+//    @JoinColumn(nullable = false, name = "board_no")
+//    private int boardNo;
+//    @ManyToOne
+//    @JoinColumn(name = "id")
+//    private Member id;
+//    @CreatedDate
+//    private LocalDateTime writeDate;
+//    @Column(nullable = false, length = 500)
+//    private String detail;
+//}
+//
+//
+//
