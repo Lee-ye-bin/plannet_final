c68c585cbb1fb8192f87287723092a3f1a07629b
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-22 10:21:02 +0900

    member entity 수정2

--- src/main/java/com/plannet/plannet/entity/Member.java
+++ src/main/java/com/plannet/plannet/entity/Member.java
@@ -1,12 +1,9 @@
 package com.plannet.plannet.entity;
 
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
-import java.sql.Date;
 import java.time.LocalDateTime;
 
 @Data
