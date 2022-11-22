6d8958f5f4c7e521676956b02284f280dd939fe6
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-22 10:55:54 +0900

    diary,quote entity 수정 friend entity 생성

--- src/main/java/com/plannet/plannet/entity/Friend.java
+++ src/main/java/com/plannet/plannet/entity/Friend.java
@@ -1,25 +1,28 @@
 package com.plannet.plannet.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.sql.Date;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
-public class Diary implements Serializable {
+public class Friend implements Serializable {
     @Id
     @ManyToOne
     @JoinColumn(name = "id")
     @Column(length = 15)
     private Member id;
     @Id
-    @CreatedDate
-    private LocalDateTime diaryDate;
-    @Column(length = 2400, nullable = false)
-    private String diary;
+    @ManyToOne
+    @JoinColumn(name = "friend_id")
+    @Column(length = 15)
+    private Member friendId;
+    @Column(name = "isaccept", length = 1, nullable = false)
+    @ColumnDefault("0")
+    private int isAccept;
 }
 
