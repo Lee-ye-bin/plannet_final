69f8fa12c026efa76b5f360fa24f68a9eaf38298
chan0823 <a3327764@naver.com>
2022-11-22 11:54:46 +0900

    SCAL entity 생성

---
+++ src/main/java/com/plannet/plannet/entity/SCAL.java
@@ -0,0 +1,23 @@
+package com.plannet.plannet.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class SCAL {
+    @Id
+    @Column(nullable = false)
+    private Long calNo;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Member id;
+
+    @Column(length = 40, nullable = false, columnDefinition = "공유 캘린더")
+    private String calName;
+
+    @Column(length = 2400)
+    private String calMemo;
+}
