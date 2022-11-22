57e709533ff95ebd1c501d9dc5746a95f75279df
cksdud0124 <winner456456@naver.com>
2022-11-22 12:12:28 +0900

    .

--- src/main/java/com/plannet/plannet/dao/SCOMRepository.java
+++ src/main/java/com/plannet/plannet/dao/SCOMRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SCOMRepository extends JpaRepository<SCOM, String> {
+public interface SCOMRepository extends JpaRepository<SCOM, Long> {
 }
