d70e3214fbec5c4bf4a4f1b063f4e6b8da7edcd1
cheuvs <cheuvs@daum.net>
2022-11-22 12:09:43 +0900

    plan 엔티티, plan 레포지토리

--- src/main/java/com/plannet/plannet/dao/PlanRepository.java
+++ src/main/java/com/plannet/plannet/dao/PlanRepository.java
@@ -2,6 +2,8 @@ package com.plannet.plannet.dao;
 
 import com.plannet.plannet.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 }
