b9a25b0006ad5a5008226f3d4b352d698870aa46
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-23 15:23:26 +0900

    MemberTest

--- src/test/java/com/plannet/plannet/entity/MemberTest.java
+++ src/test/java/com/plannet/plannet/entity/MemberTest.java
@@ -2,14 +2,19 @@ package com.plannet.plannet.entity;
 
 import com.plannet.plannet.dao.*;
 import com.plannet.plannet.vo.MemberDTO;
-import com.plannet.plannet.vo.PlanDTO;
+import com.plannet.plannet.vo.ShareDTO;
+import com.plannet.plannet.vo.WriteDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @SpringBootTest
@@ -22,11 +27,71 @@ class MemberTest {
     @Autowired
     PlanRepository planRepository;
     @Autowired
-    SMEMRepository smemRepository;
-    @Autowired
     SCALRepository scalRepository;
     @Autowired
+    SMEMRepository smemRepository;
+    @Autowired
     SPLANRepository splanRepository;
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("멤버 인서트 테스트")
+    public void createData() {
+        //Member
+        for(int i = 1; i < 11; i++) {
+            Member member = new Member();
+            member.setId("test_id_" + i);
+            member.setPwd("test_pwd_" + i);
+            member.setName("이름" + i);
+            member.setNickname("nickname" + i);
+            member.setEmail("test_" + i + "@gmail.com");
+            member.setTel("010-0000-" + i);
+            member.setJoinDate(LocalDateTime.now());
+            member.setUserCode((int) (Math.floor(Math.random() * 9999) + 1));
+
+            memberRepository.save(member);
+        }
+        //plan
+        for(int i = 1; i < 11; i++) {
+            Member member = memberRepository.findById("test_id_" + i).orElseThrow();
+            for(int j = 1; j < 11; j++) {
+                Plan plan = new Plan();
+                plan.setUserId(member);
+                plan.setPlan(i + "의 " + j + "번째 일정");
+                plan.setPlanDate(LocalDateTime.now());
+                if(j % 2 == 1) plan.setPlanChecked(1);
+                else plan.setPlanChecked(0);
+                planRepository.save(plan);
+            }
+        }
+        //scal
+        for(int i = 1; i < 6; i++){
+            SCAL scal = new SCAL();
+            scal.setCalName(i + "번의 공유 캘린더");
+            scal.setCalMemo(i + "번째 공유 캘린더의 메모입니다");
+            Member member = memberRepository.findById("test_id_" + (i*2)).orElseThrow();
+            scal.setUserId(member);
+            scalRepository.save(scal);
+        }
+        //smem
+        for(int i = 1; i < 6; i++) {
+            Long cal = 1L;
+            SMEM smemOwner = new SMEM();
+            SCAL scal = scalRepository.findById((long) i).orElseThrow();
+            smemOwner.setCalNo(scal);
+            smemOwner.setUserId(scal.getUserId());
+            smemOwner.setIsOwner(1);
+            smemRepository.save(smemOwner);
+            for(int j = 1; j < 4; j++) {
+                SMEM smem = new SMEM();
+                smemOwner.setCalNo(scal);
+                Member member = memberRepository.findById("test_id_" + j).orElseThrow();
+//                smemOwner.setUserId();
+                smemOwner.setIsOwner(1);
+                smemRepository.save(smem);
+            }
+        }
+    }
 
     // 사용자 정보&개인 일정 달성률/공유캘린더정보 불러오기
     public void navList (String userId) {
@@ -43,16 +108,22 @@ class MemberTest {
         memberDTO.setProImg(member.getProImg());
 
         //개인 일정 달성률 구하기
-        List<PlanDTO> personalTotal = planRepository.findByUserId(userId);
-        List<PlanDTO> personalEnd = planRepository.findByUserIdAndPlanChecked(userId, 1);
+        List<WriteDTO> personalTotal = planRepository.findByUserId(userId);
+        List<WriteDTO> personalEnd = planRepository.findByUserIdAndPlanChecked(userId, 1);
         int personalTotalCnt = 0; // 총 일정 갯수
         int personalEndCnt = 0; // 완료된 일정 갯수
-        for(PlanDTO e : personalTotal) {
+        for(WriteDTO e : personalTotal) {
             personalTotalCnt++;
         }
-        for(PlanDTO e : personalEnd) {
+        for(WriteDTO e : personalEnd) {
             personalEndCnt++;
         }
-        memberDTO.setPes((int)personalTotalCnt*100/personalEndCnt);
+        memberDTO.setPes(personalTotalCnt * 100 / personalEndCnt);
+
+        // 공유캘린더정보 불러오기
+        List<SMEM> smemList = smemRepository.findByUserId(member);
+        List<ShareDTO> shareDTOS = new ArrayList<>();
+        ShareDTO shareDTO = new ShareDTO();
+        memberDTO.setSCalList(shareDTOS);
     }
 }
