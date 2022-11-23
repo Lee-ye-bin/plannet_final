05c44e4c8a38570b28646106ed6f3284a45acbb8
cheuvs <cheuvs@daum.net>
2022-11-23 13:54:23 +0900

    WriteDTO 업로드

---
+++ src/main/java/com/plannet/plannet/vo/WriteDTO.java
@@ -0,0 +1,16 @@
+package com.plannet.plannet.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.sql.Date;
+import java.util.List;
+import java.util.Map;
+
+
+@Getter @Setter
+public class WriteDTO {
+    private String userId;
+    private Date date;
+    private List<Map<String, Object>> plan;
+    private String diary;
+}
