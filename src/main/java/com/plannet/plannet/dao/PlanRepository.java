8304b4fa3d08ad9ea15b8cfff0b6ed9dfde1d052
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-23 14:26:39 +0900

    MemberTest

--- src/main/java/com/plannet/plannet/dao/PlanRepository.java
+++ src/main/java/com/plannet/plannet/dao/PlanRepository.java
@@ -1,13 +1,13 @@
 package com.plannet.plannet.dao;
 
 import com.plannet.plannet.entity.Plan;
-import com.plannet.plannet.vo.PlanDTO;
+import com.plannet.plannet.vo.WriteDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
-    List<PlanDTO> findByUserId(String userId);
-    List<PlanDTO> findByUserIdAndPlanChecked(String userId, int planChecked);
+    List<WriteDTO> findByUserId(String userId);
+    List<WriteDTO> findByUserIdAndPlanChecked(String userId, int planChecked);
 }
