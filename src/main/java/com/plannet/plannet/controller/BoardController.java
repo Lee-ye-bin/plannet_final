3d2c5411a6ff499a766946787b205ae75188d113
cksdud0124 <winner456456@naver.com>
2022-11-23 12:33:41 +0900

    board 서비스

--- src/main/java/com/plannet/plannet/controller/BoardController.java
+++ src/main/java/com/plannet/plannet/controller/BoardController.java
@@ -6,18 +6,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
-@RestController
 public class BoardController {
+    // Service 로직 연결
     private BoardService boardService;
-    public BoardController(BoardService boardService) {this.boardService = boardService;}
-    @GetMapping("/GetBoard/List")
+    public BoardController(BoardService boardService) { this.boardService = boardService; }
+
+    @GetMapping("/Board/list")
+    // 전체조회기 때문에 boardList(@RequestParam) 으로 param 값을 받을 필요가 없음
     public ResponseEntity<List<BoardDTO>> boardList() {
-        List<BoardDTO> list = boardService.getBoardList('1');
+        // 서비스를 다녀옴, 결과를 리스트로 받음
+        List<BoardDTO> list = boardService.getBoardList();
         return new ResponseEntity(list, HttpStatus.OK);
     }
 }
