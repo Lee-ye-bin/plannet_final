d0ee38d8a7dc4b9f3b19b9b609e48dccc76df085
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-24 09:19:27 +0900

    board mapping 수정

--- src/main/java/com/plannet/plannet/controller/BoardController.java
+++ src/main/java/com/plannet/plannet/controller/BoardController.java
@@ -2,22 +2,24 @@ package com.plannet.plannet.controller;
 
 import com.plannet.plannet.service.BoardService;
 import com.plannet.plannet.vo.BoardDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/board")
+@Slf4j
 public class BoardController {
     // Service 로직 연결
     private BoardService boardService;
     public BoardController(BoardService boardService) { this.boardService = boardService; }
 
     // 전체 보드 리스트 불러오기
-    @GetMapping("/Board/list")
+    @GetMapping("/list")
     // 전체조회기 때문에 boardList(@RequestParam) 으로 param 값을 받을 필요가 없음
     public ResponseEntity<List<BoardDTO>> boardList() {
         // 서비스를 다녀옴, 결과를 리스트로 받음
@@ -26,7 +28,7 @@ public class BoardController {
     }
 
     // boardNo로 해당 게시물의 좋아요 수 조회하기
-    @GetMapping("/Board/likeCnt")
+    @GetMapping("/like_cnt")
     public ResponseEntity<Integer> boardList(@RequestParam int boardNo) {
         long likeCnt = boardService.getLikeCnt(boardNo);
         return new ResponseEntity(likeCnt, HttpStatus.OK);
