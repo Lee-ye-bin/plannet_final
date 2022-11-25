02f7ee3027f07e04b7561cfd814e676fdab2efce
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-25 16:05:26 +0900

    Home에 뿌려줄 정보 구현

--- src/main/java/com/plannet/plannet/entity/Quote.java
+++ src/main/java/com/plannet/plannet/entity/Quote.java
@@ -9,9 +9,13 @@ import javax.persistence.*;
 @Getter @Setter
 @ToString
 @Entity
+@SequenceGenerator(
+        name = "QUOTE_GENERATOR",
+        sequenceName = "QUOTE_SEQUENCES",
+        initialValue = 1, allocationSize = 1)
 public class Quote {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "QUOTE_GENERATOR")
     private int quoteNo;
     
     @Column(length = 500, nullable = false)
--- src/main/java/com/plannet/plannet/vo/HomeDTO.java
+++ src/main/java/com/plannet/plannet/vo/HomeDTO.java
@@ -3,7 +3,7 @@ package com.plannet.plannet.vo;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -11,5 +11,8 @@ import java.util.Set;
 @Getter @Setter
 public class HomeDTO {
     private List<List<Map<String, Object>>> weekPlan;
-    private List<Set<LocalDateTime>> calDot;
+    private List<Set<LocalDate>> planMark;
+    private String memo;
+    private String Quote;
+    private boolean isOk;
 }
