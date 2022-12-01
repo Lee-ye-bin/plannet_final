b01c2d6523c0c8246a71b88203137367568755bb
cksdud0124 <winner456456@naver.com>
2022-12-01 15:20:58 +0900

    .

--- src/main/java/com/plannet/plannet/controller/BoardController.java
+++ src/main/java/com/plannet/plannet/controller/BoardController.java
@@ -116,7 +116,7 @@ public class BoardController {
         }
     }
     // 자유게시판 댓글 작성하기
-    @GetMapping("/comment/write")
+    @GetMapping("/comment_write")
     public ResponseEntity<Integer> boardCommentsCreate(@RequestParam Long boardNo, String id, String detail) {
         boolean boardCommentsCreate = boardService.getcommentsCreate(boardNo, id, detail);
         if (boardCommentsCreate) {
@@ -127,7 +127,7 @@ public class BoardController {
     }
 
     // 자유게시판 댓글 불러오기
-    @PostMapping("/comment/load")
+    @PostMapping("/comment_load")
     public ResponseEntity<List<Map<String, Object>>> boardCommentsLoad(@RequestBody Map<Integer, Integer> boardNo) {
         int num = boardNo.get("num");
         BoardDTO boardDTO = boardService.commentsLoad(num);
